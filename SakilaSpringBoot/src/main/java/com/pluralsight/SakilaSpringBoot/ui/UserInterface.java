4446ff6e9a0cece114e4fe550e8135770519ed2e
lucas.rappatta24@outlook.com <lucas.rappatta24@outlook.com>
2025-12-09 09:35:35 -0600

    add beans

---
+++ SakilaSpringBoot/src/main/java/com/pluralsight/SakilaSpringBoot/ui/UserInterface.java
@@ -0,0 +1,13 @@
+package com.pluralsight.SakilaSpringBoot.ui;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserInterface implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Running User Interface");
+    }
+}
